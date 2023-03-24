1e4bcc207355c3296aa25f769d34d6a1de72c146
greatalazar <greatalazar@gmail.com>
2023-03-24 04:01:25 +0300

    Add test project

--- src/greatalazar.CodeUtil.Lib/CodingHelpers/CastingUtilities.cs
+++ src/greatalazar.CodeUtil.Lib/CodingHelpers/CastingUtilities.cs
@@ -4,10 +4,6 @@ namespace greatalazar.CodeUtils.Lib.CodingHelpers;
 
 public static class CastingUtilities
 {
-	#region casting helpers
-
 	[MethodImpl(MethodImplOptions.AggressiveInlining)]
 	public static T CastTo<T>(this object ip) => (T)ip;
-
-	#endregion
 }
--- src/greatalazar.CodeUtil.Lib/CodingHelpers/FileSystemUtilities.cs
+++ src/greatalazar.CodeUtil.Lib/CodingHelpers/FileSystemUtilities.cs
@@ -1,34 +1,47 @@
 ﻿using greatalazar.CodeUtils.Lib.ArgumentValidation;
-using System.Threading.Tasks.Sources;
 
 namespace greatalazar.CodeUtils.Lib.CodingHelpers;
 
 public static class FileSystemUtilities
 {
-    public static string AddSuffixToFileName(string fileName, string suffix)
+	//TODO: check if argument validation is actually necessary, to avoid double checking
+	public static string AddSuffixToFileName(string fileName, string suffix)
 	{
+		Guard.Argument(fileName, nameof(fileName)).NotNullOrEmpty();
+		Guard.Argument(suffix, nameof(suffix)).NotNullOrEmpty();
+
 		return $"{Path.GetFileNameWithoutExtension(fileName)}{suffix}{Path.GetExtension(fileName)}";
 	}
 
 	public static string AddSuffixToFilePath(string filePath, string suffix)
 	{
+		Guard.Argument(filePath, nameof(filePath)).NotNullOrEmpty();
+		Guard.Argument(suffix, nameof(suffix)).NotNullOrEmpty();
+
 		return Path.Combine(Path.GetDirectoryName(filePath), AddSuffixToFileName(filePath, suffix));
 	}
 
 	public static string ChangeFileNameExtension(string fileName, string newExt)
 	{
+		Guard.Argument(fileName, nameof(fileName)).NotNullOrEmpty();
+		Guard.Argument(newExt, nameof(newExt)).NotNullOrEmpty();
+
 		return Path.GetFileNameWithoutExtension(fileName) + newExt;
 	}
 
 	public static string ChangeFilePathExtension(string filePath, string newExt)
 	{
 		Guard.Argument(filePath, nameof(filePath)).NotNullOrEmpty();
+		Guard.Argument(newExt, nameof(newExt)).NotNullOrEmpty();
 
 		return Path.Combine(Path.GetDirectoryName(filePath), ChangeFileNameExtension(filePath, newExt));
 	}
 
 	public static string ChangeFileDirectory(string filePath, string newDirectory)
 	{
+		Guard.Argument(filePath, nameof(filePath)).NotNullOrEmpty();
+		Guard.Argument(newDirectory, nameof(newDirectory)).NotNullOrEmpty();
+
 		return Path.Combine(newDirectory, Path.GetFileName(filePath));
 	}
 
