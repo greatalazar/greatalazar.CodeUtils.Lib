04511d9969b80d5dda227b64ddf4d9b068579b02
greatalazar <greatalazar@gmail.com>
2023-11-01 14:47:08 +0300

    Create RandomUtilities.cs

---
+++ src/greatalazar.CodeUtils.Lib/CodingHelpers/RandomUtilities.cs
@@ -0,0 +1,10 @@
+﻿namespace greatalazar.CodeUtils.Lib.CodingHelpers;
+
+public static class RandomUtilities
+{
+	public static float NextSingle(this Random random, float minValue, float maxValue) =>
+		random.NextSingle() * (maxValue - minValue) + minValue;
+
+	public static double NextDouble(this Random random, double minValue, double maxValue) =>
+		random.NextDouble() * (maxValue - minValue) + minValue;
+}
